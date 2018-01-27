02b13d72eb88928108d98028dec15185a05e6651
Nate Wingert <nate.wingert.dev@gmail.com>
2018-01-26 17:38:02 -0700

    initial commit

---
+++ ballclock.go
@@ -0,0 +1,17 @@
+// update loop where each iteration represents one ball / minute
+// loop operations:
+//
+
+package ballclock
+
+import "time"
+
+func simBalls(ballCount int) (int, int) {
+	var days int
+	var time time.Duration
+
+}
+
+func simClock(ballCount, minutes int) int {
+
+}
